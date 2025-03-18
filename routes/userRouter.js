a21fe0da2544fc98b6dde0099cac8fc3b723fd78
Студент КСТ <student@std5.local>
2025-03-18 15:39:06 +0300

    first commit

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const CheckRoleMiddleware = require("../middleware/CheckRoleMiddleware");
 
 
-router.post("/create", authMiddleware, UserController.create)
+router.post("/create",  UserController.create)
 router.post("/login", UserController.login)
 router.get("/all", UserController.getAll)
 router.get("/auth",  UserController.check)
